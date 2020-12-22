206b3b15c770f92494bcfbc0254cb2ec55df44d3
ANUBHAVNATANI <anubhavnatani99@gmail.com>
2020-12-23 03:14:23 +0530

    Add diverging bar chart

--- divergingBarChart.js
+++ divergingBarChart.js
@@ -1,5 +1,7 @@
 d3.csv("./Data/drop_out_rate.csv").then(showData);
 function showData(data) {
+  const bodyHeight = 500;
+  const bodyWidth = 1000;
   let container = d3.select("#divergingBarChart");
   let states = {};
   for (let { State_UT, year, Avg } of data) {
@@ -10,26 +12,77 @@ function showData(data) {
     }
   }
 
+  let stateData = [];
   for (let state in states) {
     stateObj = states[state];
-    absoluteChange = stateObj["2014-15"] - stateObj["2012-13"];
-    percentChange =
-      ((stateObj["2014-15"] - stateObj["2012-13"]) / stateObj["2012-13"]) * 100;
+    let year1 = stateObj["2012-13"] ? stateObj["2012-13"] : stateObj["2013-14"];
+    let year2 = stateObj["2014-15"];
+
+    let absoluteChange = year2 - year1;
+    let percentChange = ((year2 - year1) / year1) * 100;
+
     absoluteChange = +absoluteChange.toFixed(2);
     percentChange = +percentChange.toFixed(2);
-    states[state] = { ...stateObj, absoluteChange, percentChange };
+    stateData.push({ state, absoluteChange, percentChange });
   }
-
+  console.log(stateData);
   //diverging bar chart code
+  let margin = { top: 30, right: 60, bottom: 10, left: 60 };
+  let barHeight = 25;
+  let height =
+    Math.ceil((stateData.length + 0.1) * barHeight) +
+    margin.top +
+    margin.bottom;
+
+  //let format = d3.format("+,.0%");
+  xAxis = (g) =>
+    g
+      .attr("transform", `translate(0,${margin.top})`)
+      .call(
+        d3
+          .axisTop(x)
+          .ticks(bodyWidth / 80)
+          .tickFormat((d) => d + "%")
+      )
+      .call((g) => g.select(".domain").remove());
+
+  yAxis = (g) =>
+    g
+      .attr("transform", `translate(${x(0)},0)`)
+      .call(
+        d3
+          .axisLeft(y)
+          .tickFormat((i) => stateData[i].state)
+          .tickSize(0)
+          .tickPadding(6)
+      )
+      .call((g) =>
+        g
+          .selectAll(".tick text")
+          .filter((i) => stateData[i].percentChange < 0)
+          .attr("text-anchor", "start")
+          .attr("x", 6)
+      );
+
+  x = d3
+    .scaleLinear()
+    .domain(d3.extent(stateData, (d) => d.percentChange))
+    .rangeRound([margin.left, bodyWidth - margin.right]);
+  y = d3
+    .scaleBand()
+    .domain(d3.range(stateData.length))
+    .rangeRound([margin.top, height - margin.bottom])
+    .padding(0.1);
+
   container
     .append("g")
     .selectAll("rect")
-    .data(states)
+    .data(stateData)
     .join("rect")
-    .attr("fill", d => d3.schemeSet1[d.value > 0 ? 1 : 0])
-    .attr("x", d => x(Math.min(d.value, 0)))
+    .attr("fill", (d) => d3.schemeSet1[d.percentChange > 0 ? 1 : 0])
+    .attr("x", (d) => x(Math.min(d.percentChange, 0)))
     .attr("y", (d, i) => y(i))
-    .attr("width", d => Math.abs(x(d.value) - x(0)))
+    .attr("width", (d) => Math.abs(x(d.percentChange) - x(0)))
     .attr("height", y.bandwidth());
 
   container
@@ -37,66 +90,15 @@ function showData(data) {
     .attr("font-family", "sans-serif")
     .attr("font-size", 10)
     .selectAll("text")
-    .data(states)
+    .data(stateData)
     .join("text")
-    .attr("text-anchor", d => (d.value < 0 ? "end" : "start"))
-    .attr("x", d => x(d.value) + Math.sign(d.value - 0) * 4)
+    .attr("text-anchor", (d) => (d.percentChange < 0 ? "end" : "start"))
+    .attr("x", (d) => x(d.percentChange) + Math.sign(d.percentChange - 0) * 4)
     .attr("y", (d, i) => y(i) + y.bandwidth() / 2)
     .attr("dy", "0.35em")
-    .text(d => format(d.value));
+    .text((d) => d.percentChange + "%");
 
   container.append("g").call(xAxis);
 
   container.append("g").call(yAxis);
-
-  return container.node();
-  ///new code
-  x = d3
-    .scaleLinear()
-    .domain(d3.extent(data, d => d.value))
-    .rangeRound([margin.left, width - margin.right]);
-
-  y = d3
-    .scaleBand()
-    .domain(d3.range(data.length))
-    .rangeRound([margin.top, height - margin.bottom])
-    .padding(0.1);
-
-  xAxis = g =>
-    g
-      .attr("transform", `translate(0,${margin.top})`)
-      .call(
-        d3
-          .axisTop(x)
-          .ticks(width / 80)
-          .tickFormat(tickFormat)
-      )
-      .call(g => g.select(".domain").remove());
-
-  yAxis = g =>
-    g
-      .attr("transform", `translate(${x(0)},0)`)
-      .call(
-        d3
-          .axisLeft(y)
-          .tickFormat(i => data[i].name)
-          .tickSize(0)
-          .tickPadding(6)
-      )
-      .call(g =>
-        g
-          .selectAll(".tick text")
-          .filter(i => data[i].value < 0)
-          .attr("text-anchor", "start")
-          .attr("x", 6)
-      );
-
-  format = d3.format(metric === "absolute" ? "+,d" : "+,.0%");
-
-  tickFormat = metric === "absolute" ? d3.formatPrefix("+.1", 1e6) : format;
-
-  barHeight = 25;
-
-  height =
-    Math.ceil((data.length + 0.1) * barHeight) + margin.top + margin.bottom;
 }
